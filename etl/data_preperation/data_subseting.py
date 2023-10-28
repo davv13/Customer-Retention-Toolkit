8f9cb7ad3d94d707cb343b4c94a156c0fa3c1456
TigranBoynagryan <tigran.boynagryan@gmail.com>
2023-10-28 21:22:18 +0400

    added new files

--- etl/data_preperation/data_subseting.py
+++ etl/data_preperation/data_subseting.py
@@ -4,4 +4,4 @@ def data_State(df):
     return pd.DataFrame(df.loc[:,'State'])
 
 def data_PlanDetails(df):
-    return pd.DataFrame(df.loc[:,])
+    return pd.DataFrame(df.loc[:,['Area Code','International Code']])
